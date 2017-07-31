7847162d0c23572553f3315dbdc6a6cc87cda5c3
Harry Cummings <harry.cummings@softwire.com>
2017-07-31 17:18:31 +0100

    Update piston graphics

--- src/view/graphics/piston.js
+++ src/view/graphics/piston.js
@@ -1,30 +1,39 @@
 import {unit, preRender} from './common';
 
-export const inactive = preRender((ctx: CanvasRenderingContext2D) => {
+const drawHousing = (ctx: CanvasRenderingContext2D) => {
     const gradient = ctx.createLinearGradient(-unit / 3, 0, unit / 3, 0);
     gradient.addColorStop(0, '#666');
     gradient.addColorStop(0.5, '#999');
     gradient.addColorStop(1, '#666');
 
     ctx.fillStyle = gradient;
-    ctx.fillRect(-unit/3, -unit / 2, unit * 2 / 3, unit * 7/8);
+    ctx.fillRect(-unit/3, -unit * 3/8, unit * 2 / 3, unit * 7/8);
+};
+
+const drawHead = (ctx: CanvasRenderingContext2D, extension:number) => {
+    const gradient = ctx.createLinearGradient(-unit / 3, 0, unit / 3, 0);
+    gradient.addColorStop(0, '#999');
+    gradient.addColorStop(0.5, '#ccc');
+    gradient.addColorStop(1, '#999');
+
+    ctx.fillStyle = gradient;
+    ctx.fillRect(-unit/3, extension, unit * 2 / 3, unit / 8);
+
+    if (extension < -unit/2) {
+        ctx.fillRect(-unit / 16, extension, unit / 8, -unit * 3/8 - extension);
+    }
+}
+
+export const inactive = preRender((ctx: CanvasRenderingContext2D) => {
+    drawHousing(ctx);
+    drawHead(ctx, -unit / 2);
 });
 
 export const activeFrames = [];
 
 for (let i = 0; i < 4; ++i) {
     activeFrames.push(preRender((ctx: CanvasRenderingContext2D) => {
-        ctx.strokeStyle = '#333';
-        ctx.lineCap = 'round';
-        ctx.lineWidth = unit / 16;
-
-        const extension = -unit * (6 - i) / 4;
-        ctx.moveTo(0, -unit/2);
-        ctx.lineTo(0, extension);
-        ctx.moveTo(-unit * 3/8, extension);
-        ctx.lineTo(unit * 3/8, extension);
-        ctx.stroke();
-
-        inactive(ctx);
+        drawHousing(ctx);
+        drawHead(ctx, -unit * (6 - i) / 4);
     }, unit, unit * 3));
 }
